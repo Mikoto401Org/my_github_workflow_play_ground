fb8f24a915f74a547bb5dc0d603ca18138fd7141
mikoto <daniel.jung@posteo.de>
2022-10-15 21:56:37 +0200

    init

---
+++ src/main/java/de/mikoto/GithubWorkflowTestApplication.java
@@ -0,0 +1,13 @@
+package de.mikoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubWorkflowTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubWorkflowTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/mikoto/GithubWorkflowTestApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mikoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubWorkflowTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
